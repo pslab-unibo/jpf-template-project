5fc709f19544e7a948b71b1b0c500a9d3502becd
Gianluca Aguzzi <gianluca.aguzzi@unibo.it>
2025-03-03 14:27:32 +0100

    fix: add ALL-UNNAMED to load Random in JPF

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,12 @@ File(rootProject.rootDir.path + searchingPath).listFiles()
                 }
             )
             main = "-jar"
-            args = listOf("./jpf-runner/build/RunJPF.jar", ".${searchingPath}" + file.name)
+            args = listOf(
+                "-jar",
+                "--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED",
+                "./jpf-runner/build/RunJPF.jar", ".${searchingPath}" + file.name
+            )
+            //args = listOf("./jpf-runner/build/RunJPF.jar", ".${searchingPath}" + file.name)
         }
         val capitalizedName = it.nameWithoutExtension.capitalize()
         val jpfVerification by launchVerificationTask("run${capitalizedName}Verify", it)
